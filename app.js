9cf4a3cfc323d5e443fa32883a7e57a1d1a8c915
Cam <you@example.com>
2024-02-11 21:41:57 -0800

    Adding some of the CSS to Javascript for organization.

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-const menuToggle = document.querySelector('.toggle')
-menuToggle.addEventListener('click', () =>{
-    menuToggle.classList.toggle('active')
-})
-
+const menuToggle = document.querySelector('.toggle');
 const hamCheckbox = document.getElementById('ham');
 const navMenu = document.getElementById('menu');
 const navLinks = document.querySelectorAll('nav a');
 
-hamCheckbox.addEventListener('change', function() {
-    if (this.checked) {
+
+menuToggle.addEventListener('click', () =>{
+    if (!menu.classList.contains("active")) {
+        menu.classList.add("active");
         navMenu.style.transform = 'translateY(0)';
+        menuToggle.querySelector('span:nth-child(1)').style.width = '40px';
+        menuToggle.querySelector('span:nth-child(1)').style.transform = 'translateY(0px) rotate(45deg)';
+        menuToggle.querySelector('span:nth-child(2)').style.width = '40px';
+        menuToggle.querySelector('span:nth-child(2)').style.transform = 'translateY(0px) rotate(315deg)';
+        menuToggle.querySelector('span:nth-child(3)').style.transform = 'translateX(60px)';
         navLinks.forEach(link => {
-            link.style.opacity = 1;
-            link.style.transition = 'ease-in-out .6s';
+        link.style.opacity = 1;
+        link.style.transition = 'ease-in-out .6s';
         });
-    } else {
+      } else {
+        menu.classList.remove("active");
         navMenu.style.transform = 'translateY(-200%)';
+        menuToggle.querySelector('span:nth-child(1)').style.width = '';
+        menuToggle.querySelector('span:nth-child(1)').style.transform = '';
+        menuToggle.querySelector('span:nth-child(2)').style.width = '';
+        menuToggle.querySelector('span:nth-child(2)').style.transform = '';
+        menuToggle.querySelector('span:nth-child(3)').style.transform = '';
         navLinks.forEach(link => {
-            link.style.opacity = 0;
-            link.style.transition = 'ease-in-out .6s';
+        link.style.opacity = 0;
+        link.style.transition = 'ease-in-out .6s';
         });
-    }
-});
+      }
+})
+
